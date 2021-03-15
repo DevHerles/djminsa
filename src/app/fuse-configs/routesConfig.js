0558e0aae6d0abc50af3a32eede810602791b3b4
HerlesINC <herles.incalla@gmail.com>
2021-03-14 19:01:37 -0500

    Add auth module

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -1,32 +1,52 @@
 import React from 'react';
-import {Redirect} from 'react-router-dom';
-import {FuseUtils} from '@fuse';
-import {ExampleConfig} from 'app/main/example/ExampleConfig';
-import {appsConfig} from 'app/main/apps/appsConfig';
-import {pagesConfig} from 'app/main/pages/pagesConfig';
-import {LoginConfig} from 'app/main/login/LoginConfig';
-// import {RegisterConfig} from 'app/main/register/RegisterConfig';
-import {LogoutConfig} from 'app/main/logout/LogoutConfig';
+import {
+  Redirect
+} from 'react-router-dom';
+import {
+  FuseUtils
+} from '@fuse';
+import {
+  appsConfig
+} from 'app/main/apps/appsConfig';
+import {
+  pagesConfig
+} from 'app/main/pages/pagesConfig';
+import {
+  LoginConfig
+} from 'app/main/login/LoginConfig';
+import {
+  LogoutConfig
+} from 'app/main/logout/LogoutConfig';
+import {
+  authRoles
+} from 'app/auth';
+import _ from 'lodash';
+
+function setAdminAuth(configs) {
+  return configs.map(config => _.merge({}, config, {
+    auth: authRoles.admin
+  }))
+}
 
 const routeConfigs = [
+  ...setAdminAuth([
     ...appsConfig,
     ...pagesConfig,
-    ExampleConfig,
-    LoginConfig,
-    // RegisterConfig,
     LogoutConfig,
+  ]),
+  LoginConfig,
 ];
 
 const routes = [
-    ...FuseUtils.generateRoutesFromConfigs(routeConfigs),
-    {
-        path: '/',
-        exact: true,
-        component: () => <Redirect to="/apps/profiles"/>
-    },
-    {
-        component: () => <Redirect to="/pages/errors/error-404"/>
-    }
+  ...FuseUtils.generateRoutesFromConfigs(routeConfigs),
+  {
+    path: '/',
+    exact: true,
+    component: () => < Redirect to = "/apps/profiles" / >
+  },
+  {
+    component: () => < Redirect to = "/pages/errors/error-404" / >
+  }
 ];
 
 export default routes;
